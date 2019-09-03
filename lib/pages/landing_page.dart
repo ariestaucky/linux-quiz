235b73319d398b35a63986b5ccfa288f2172bffc
ariesta <ariestaucky@gmail.com>
2019-09-03 11:32:01 +0700

    change landing page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -7,8 +7,8 @@ class LandingPage extends StatefulWidget {
   _LandingPageState createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> with TickerProviderStateMixin {
-
+class _LandingPageState extends State<LandingPage>
+    with TickerProviderStateMixin {
   void start() {
     Navigator.push(
       context,
@@ -24,25 +24,13 @@ class _LandingPageState extends State<LandingPage> with TickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.greenAccent,
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("linux.jpg"), fit: BoxFit.cover)
-        ),
-        child: StartButton(start)
-      )
-      // new InkWell(
-      //   onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new QuizPage())),
-      //   child: new Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       new Text("Let's Start!", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
-      //       new Text("Tap to start!", style: new TextStyle(color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),)
-      //     ],
-      //   ),
-      // ),
-    );
+        backgroundColor: Colors.greenAccent,
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("linux.jpg"), fit: BoxFit.cover)),
+            child: StartButton(start)));
   }
 }
